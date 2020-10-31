f7e11394d28a1d20536605ec529e960194cfca2c
Shawn Contant <shawnc366@gmail.com>
2020-10-30 21:05:35 -0500

    Move alert component to modern structure

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -2,23 +2,21 @@ import React, {Component} from 'react';
 import {getErrorMsg} from '../../actions/userActions';
 import {connect} from 'react-redux';
 
-class ConnectedAlert extends Component {
+class Alert extends Component {
     constructor(props) {
-        super(props);       
+        super(props);
     }
-
-    componentDidMount(){
-        this.props.dispatch(getErrorMsg());
-        console.log(this.props)
-    }
-
+    
     render() {
-        console.log(this.props)
-        return <div></div>
+        const {errorMsg} = this.props;
+        return <div>{errorMsg}</div>
     }
 }
-const Alert = connect((store) => {
-    console.log(store)
-    return store.user.errorMsg
-})(ConnectedAlert);
-export default Alert;
+
+const mapStateToProps = ({error}) => {
+    return {
+        errorMsg: error.errorMsg
+    }
+}
+
+export default connect(mapStateToProps, null)(Alert);
