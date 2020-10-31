95cb55806f7fc8c5f48cae01662b5821b1f42af3
Shawn Contant <shawnc366@gmail.com>
2020-10-30 21:02:28 -0500

    Add new api file that will be used in refactored redux

--- src/api.js
+++ src/api.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
+import {api_url} from './config.json';
 
-export class Request {
-    static get(url)
+export default class Request {
+    static get(url, data) {
+        return axios.get(api_url + url, data);
+    }
+
+    static post(url, data) {
+        return axios.get(api_url + url, data);
+    }
+
+    static put(url, data) {
+        return axios.put(api_url + url, data);
+    }
+
+    static del(url, data) {
+        return axios.delete(api_url + url, data);
+    }
 }
