8708efcd3d338b4bf88e67a57b0cdb7b0359c820
Shawn Contant <shawnc366@gmail.com>
2020-11-18 18:47:01 -0600

    Start setting up Sequelize usage

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const sqlite3 = require('sqlite3').verbose();
 const app = express();
 const cors = require('cors');
 
+const Building = require('./server/api/building');
+const Class = require('./server/api/class');
+const Major = require('./server/api/major');
+const Pages = require('./server/api/pages');
+const Room = require('./server/api/room');
+const Section = require('./server/api/section');
+const User = require('./server/api/user');
+
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.static('public'));
 // app.use(function(req, res, next) {
@@ -14,6 +22,14 @@ app.use(express.static('public'));
 // });
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use(Building);
+app.use(Class);
+app.use(Major);
+app.use(Pages);
+app.use(Room);
+app.use(Section);
+app.use(User);
 
 var db = new sqlite3.Database('server/db/campus-react.db');
 
